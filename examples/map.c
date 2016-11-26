01dce31f7d973d82df0dac3e239adc1e61a80d86
baltoli <bsc28@cam.ac.uk>
2016-11-26 18:08:48 +0000

    Fix util

--- examples/map.c
+++ examples/map.c
@@ -15,25 +15,6 @@
  * introducing parallelism into this idiom.
  */
 
-void range_fill(int *arr, size_t n) {
-  for(int i = 0; i < n; i++) {
-    arr[i] = i;
-  }
-}
-
-void print_array(int *arr, size_t n) {
-  printf("[");
-  for(size_t i = 0; i < n - 1; i++) {
-    printf("%d, ", arr[i]);
-  }
-
-  if(n > 0) {
-    printf("%d]\n", arr[n-1]);
-  } else {
-    printf("]\n");
-  }
-}
-
 int id(int value) {
   return value;
 }
