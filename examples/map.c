7f9ac56ceed10ee28a58a2b3e270c95b909c0bd3
Bruce Collie <bsc28@cam.ac.uk>
2016-11-26 18:07:14 +0000

    Benches

--- examples/map.c
+++ examples/map.c
@@ -39,11 +39,7 @@ int id(int value) {
 }
 
 int square(int value) {
-  int tmp = 0;
-  for(int i = 0; i < 50; i++) {
-    tmp += rand();
-  }
-  return tmp;
+  return value * value;
 }
 
 int succ(int value) {
@@ -57,7 +53,7 @@ void map_seq(int (*work)(int), int *arr, int *out, size_t n) {
 }
 
 void map_par(int (*work)(int), int *arr, int *out, size_t n) {
-  #pragma omp parallel for num_threads(2)
+  #pragma omp parallel for
   for(size_t i = 0; i < n; i++) {
     out[i] = work(arr[i]);
   }
@@ -65,28 +61,28 @@ void map_par(int (*work)(int), int *arr, int *out, size_t n) {
 
 
 int main(void) {
-  const size_t size = 250000;
+  const size_t size = 1000000;
 
-  clock_t fill_start = clock();
-  int seq_nums[size];
+  double fill_start = omp_get_wtime();
+  int *seq_nums = malloc(size * sizeof(int));
   int *seq_out = malloc(size * sizeof(int));
   range_fill(seq_nums, size);
 
-  int par_nums[size];
+  int *par_nums = malloc(size * sizeof(int));
   int *par_out = malloc(size * sizeof(int));
   range_fill(par_nums, size);
-  clock_t fill_end = clock();
-  printf("Array (%zu) fill time: %fs\n", size, (double)(fill_end - fill_start) / CLOCKS_PER_SEC);
+  double fill_end = omp_get_wtime();
+  printf("Array (%zu) fill time: %fs\n", size, (double)(fill_end - fill_start));
 
-  clock_t par_start = clock();
+  double par_start = omp_get_wtime();
   for(int i = 0; i < 100; i++) map_par(&square, par_nums, par_out, size);
-  clock_t par_end = clock();
-  printf("Parallel run time: %fs\n", (double)(par_end - par_start) / CLOCKS_PER_SEC);
+  double par_end = omp_get_wtime();
+  printf("Parallel run time: %fs\n", (double)(par_end - par_start));
 
-  clock_t seq_start = clock();
+  double seq_start = omp_get_wtime();
   for(int i = 0; i < 100; i++) map_seq(&square, seq_nums, seq_out, size);
-  clock_t seq_end = clock();
-  printf("Sequential run time: %fs\n", (double)(seq_end - seq_start) / CLOCKS_PER_SEC);
+  double seq_end = omp_get_wtime();
+  printf("Sequential run time: %fs\n", (double)(seq_end - seq_start));
 
   return 0;
 }
