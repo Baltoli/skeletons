92acc17c10187a0843ce4ab1ac1a69ea8bb76d0a
baltoli <bsc28@cam.ac.uk>
2016-11-27 16:36:50 +0000

    Hack build together again

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,20 @@
+#include <iostream>
+#include <string>
+
 #include <clang/Tooling/Tooling.h>
 #include <clang/Tooling/CommonOptionsParser.h>
+#include <llvm/Support/CommandLine.h>
 
-using namespace clang;
-using namespace clang::driver;
+using namespace llvm;
 using namespace clang::tooling;
+using std::string;
 
-static llvm::cl::OptionCategory ToolingSampleCategory("Tooling Sample");
+static cl::OptionCategory OptionsCategory("Options");
 
 int main(int argc, const char **argv) {
-  CommonOptionsParser op(argc, argv, ToolingSampleCategory);
+  CommonOptionsParser OptionsParser(argc, argv, OptionsCategory);
+  for(auto s : OptionsParser.getSourcePathList()) {
+    std::cout << s << std::endl;
+  }
   return 0;
 }
