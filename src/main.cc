fba6da49484816383b40c7149ec88b00d7c0bf27
baltoli <bsc28@cam.ac.uk>
2016-11-27 17:24:27 +0000

    Implement action that prints file names

--- src/main.cc
+++ src/main.cc
@@ -3,18 +3,29 @@
 
 #include <clang/Tooling/Tooling.h>
 #include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/AST/ASTConsumer.h>
 #include <llvm/Support/CommandLine.h>
 
 using namespace llvm;
+using namespace clang;
 using namespace clang::tooling;
 using std::string;
 
-static cl::OptionCategory OptionsCategory("Options");
+class MyFrontendAction : public ASTFrontendAction {
+  public:
+    MyFrontendAction() {}
+
+    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef InFile) {
+      std::cout << InFile.str() << std::endl;
+      return nullptr;
+    }
+};
+
+static cl::OptionCategory OptionsCategory("skel options");
 
 int main(int argc, const char **argv) {
-  CommonOptionsParser OptionsParser(argc, argv, OptionsCategory);
-  for(auto s : OptionsParser.getSourcePathList()) {
-    std::cout << s << std::endl;
-  }
-  return 0;
+  CommonOptionsParser op(argc, argv, OptionsCategory);
+  ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+  return Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
 }
