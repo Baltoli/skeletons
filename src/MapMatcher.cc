59b7e2103196c14170a90a8886c8cf8ebe1b6c56
baltoli <bsc28@cam.ac.uk>
2016-11-28 21:43:25 +0000

    Find any for loops and dump them

--- src/MapMatcher.cc
+++ src/MapMatcher.cc
@@ -1,12 +1,20 @@
 #include "MapMatcher.hh"
 
+using namespace clang;
 using namespace clang::ast_matchers;
 
 MapHandler::MapHandler() {}
 
 void MapHandler::run(const MatchFinder::MatchResult &Result) {
+  if(const ForStmt *forS = Result.Nodes.getNodeAs<ForStmt>("for")) {
+    forS->dump();
+  }
 }
 
 StatementMatcher MapHandler::matcher() {
-  return forStmt().bind("for");
+  return(
+    forStmt(
+      hasLoopInit(stmt())
+    ).bind("for")
+  );
 }
