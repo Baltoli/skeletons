3de5766ba03c48abd8a21245e6302b72a6ea53d9
baltoli <bsc28@cam.ac.uk>
2016-11-29 11:34:48 +0000

    Nicer dumping of AST

--- src/MapMatcher.cc
+++ src/MapMatcher.cc
@@ -1,5 +1,7 @@
 #include "MapMatcher.hh"
 
+#include <llvm/Support/raw_ostream.h>
+
 using namespace clang;
 using namespace clang::ast_matchers;
 
@@ -7,14 +9,18 @@ MapHandler::MapHandler() {}
 
 void MapHandler::run(const MatchFinder::MatchResult &Result) {
   if(const ForStmt *forS = Result.Nodes.getNodeAs<ForStmt>("for")) {
-    forS->dump();
+    forS->dumpPretty(*Result.Context);
+    forS->dumpColor();
+    llvm::errs() << '\n';
   }
 }
 
 StatementMatcher MapHandler::matcher() {
   return(
     forStmt(
-      hasLoopInit(loopInitMatcher())
+      hasLoopInit(loopInitMatcher()),
+      hasCondition(loopConditionMatcher()),
+      hasIncrement(loopIncrementMatcher())
     ).bind("for")
   );
 }
@@ -25,4 +31,12 @@ StatementMatcher MapHandler::loopInitMatcher() {
       hasInitializer(ignoringParenImpCasts(integerLiteral(equals(0)))
     ))))
   );
+}
+
+StatementMatcher MapHandler::loopConditionMatcher() {
+  return expr();
+}
+
+StatementMatcher MapHandler::loopIncrementMatcher() {
+  return stmt();
 }
--- src/MapMatcher.hh
+++ src/MapMatcher.hh
@@ -9,12 +9,44 @@ using namespace clang::ast_matchers;
 class MapHandler : public MatchFinder::MatchCallback {
   public:
     MapHandler();
+
+    /**
+     * Callback method that is run when we match a result in the AST.
+     */
     virtual void run(const MatchFinder::MatchResult &Result);
 
+    /**
+     * Clang AST matcher that matches any for-loop with structure recognisable
+     * as being a map over a collection.
+     *
+     * The matching properties of this matcher are currently defined entirely in
+     * terms of its child matchers.
+     */
     StatementMatcher matcher();
 
   private:
+    /**
+     * Clang AST matcher that matches for-loop initialization statements that
+     * could be part of a map operation.
+     *
+     * This will match any loop initialisation that:
+     *  - Declares a single variable
+     *  - Is of an integer type after implicit / parenthesized casts
+     *  - Initialises the variable to constant 0
+     */
     StatementMatcher loopInitMatcher();
+
+    /**
+     * Clang AST matcher that matches for-loop conditions that could be part of
+     * a map operation.
+     */
+    StatementMatcher loopConditionMatcher();
+    
+    /**
+     * Clang AST matcher that matches for-loop increments that could be part of
+     * a map operation.
+     */
+    StatementMatcher loopIncrementMatcher();
 };
   
 
