1798b634514a8b35a47e3936cce7ce31ceb2e93e
Bruce Collie <bsc28@cam.ac.uk>
2016-12-11 11:56:39 +0000

    Change filecheck line for map 6

--- shapes/map/map_6.c
+++ shapes/map/map_6.c
@@ -3,8 +3,7 @@ int f(int);
 int main(void) {
   int *x, *y;
 
-  // CHECK: Found
-  // CHECK: line [[@LINE+1]]
+  // CHECK: Near miss
   for(int i = 0; i < 100; i++) {
     x[9] = 45;
     x[i] = f(y[i]);
