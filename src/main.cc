8331ef7898676cf9ff9c192ca5aff99b2e55e9a9
Bruce Collie <bsc28@cam.ac.uk>
2016-12-27 18:47:52 +0000

    Command line mode interface

--- src/main.cc
+++ src/main.cc
@@ -14,22 +14,34 @@ using namespace clang;
 using namespace clang::tooling;
 using std::string;
 
+enum ProgramMode {
+  Static,
+  FindReorderable
+};
+
 static cl::OptionCategory OptionsCategory("skel options");
-static cl::opt<bool> Static("static", cl::desc("Perform static analysis"),
-                            cl::cat(OptionsCategory));
 static cl::opt<bool> Overwrite("overwrite", cl::desc("Overwrite source files"), 
                                cl::cat(OptionsCategory));
 static cl::alias OverwriteA("w", cl::desc("Alias for -overwrite"), cl::aliasopt(Overwrite));
+
+static cl::opt<ProgramMode> Mode(cl::desc("choose application mode"),
+    cl::values(
+      clEnumVal(Static, "Statically analyse the program"),
+      clEnumVal(FindReorderable, "Dynamically find reorderable loops"),
+      nullptr
+    ), cl::cat(OptionsCategory), cl::Required);
 
 int main(int argc, const char **argv) {
   CommonOptionsParser op(argc, argv, OptionsCategory);
   ClangTool Tool(op.getCompilations(), op.getSourcePathList());
   
-  if(Static) {
+  if(Mode == Static) {
     if(Overwrite) {
-      return Tool.run(newFrontendActionFactory<AnnotateSkeletonsAction>().get());
+      return Tool.run(
+          newFrontendActionFactory<AnnotateSkeletonsAction>().get());
     } else {
-      return Tool.run(newFrontendActionFactory<DetectSkeletonsAction>().get());
+      return Tool.run(
+          newFrontendActionFactory<DetectSkeletonsAction>().get());
     }
   }
 
