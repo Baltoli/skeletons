c59f5c1fef0c34715622a145b946462d6c8b53b7
Bruce Collie <bsc28@cam.ac.uk>
2016-12-27 18:09:32 +0000

    Add match finder to dynamic consumer

--- src/DetectDynamicASTConsumer.hh
+++ src/DetectDynamicASTConsumer.hh
@@ -2,14 +2,18 @@
 #define DETECT_DYNAMIC_AST_CONSUMER_H
 
 #include <clang/AST/ASTConsumer.h>
-
-#include "MapMatcher.hh"
+#include <clang/ASTMatchers/ASTMatchers.h>
+#include <clang/ASTMatchers/ASTMatchFinder.h>
 
 using namespace clang;
+using namespace clang::ast_matchers;
+
 class DetectDynamicASTConsumer : public ASTConsumer {
   public:
     DetectDynamicASTConsumer();
     void HandleTranslationUnit(ASTContext &context);
+  private:
+    MatchFinder Matcher;
 };
 
 #endif
